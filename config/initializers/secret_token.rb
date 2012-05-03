8348d25648f887fbdf112ea6e165ace9070c677b
Ajay kumar Guthikonda <aroopchandra@gmail.com>
2012-05-02 20:05:57 -0700

    Added empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nrimovies::Application.config.secret_token = 'd4a9b31d9cd57cd2cc9a6d2b2e2cb3bb0b68bb962234d33f45bc34ecfde87400284c9179597326c6257f4cabd1dc0f977f9c49849bd8c693f40e7d4bd439c4a6'
